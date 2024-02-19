737a85ecfbf15532c102074d21b82eb40cc63d45
Manisha Kumari <manisha.tkg08@gmail.com>
2024-02-19 12:19:04 +0530

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import data from './data.json'
+import Question from './components/Question'
+import Option from './components/Option'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [selectedIndex, setSelectedIndex] = useState(null)
+  const options = data[count].options
+  const corrrectAnswer = data[count].correctAnswer
+  
+  const handleButtonClick = () => {
+    if (count === 3) {
+      setCount(0)
+    } else {
+      setCount(count + 1)
+    }
+    setSelectedIndex(null)
+  }
+  const handleOptionClick = (index) => {
+    setSelectedIndex(index)
+  }
+
+  return (
+    <div>
+      <h1 className='header'>Welcome To My Quiz App</h1>
+      <Question text={data[count].question} />
+      {options.map((option, index) => {
+        return (<Option
+          key={index}
+          option={option}
+          index={index}
+          handleOptionClick={handleOptionClick}
+          selectedIndex={selectedIndex}
+          correctAnswer={corrrectAnswer} />)
+      })
+      }
+      <button onClick={handleButtonClick}>Next</button>
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/components/Question.jsx
@@ -0,0 +1,7 @@
+const Question = ({ text }) => {
+    return (
+        <h2>{text}</h2>
+    )
+}
+
+export default Question

f9cea68ad95af3fa433a133aed2726a37290a311
Manisha Kumari <manisha.tkg08@gmail.com>
2024-02-27 11:30:08 +0530

    add styling in the options-cards

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -2,7 +2,7 @@ const Option = ({ option, index, handleOptionClick, selectedIndex, correctAnswer
     const clicked = selectedIndex !== null && index === correctAnswer
     const notIsCorrect = selectedIndex === index && selectedIndex !== correctAnswer
     return (
-        <div>
+        <div className="optionDiv">
             <div className={clicked ? "green"
                 : notIsCorrect ? "red" : "white"} onClick={() => handleOptionClick(index)}>{option}</div>
         </div>
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,6 +1,6 @@
 const Question = ({ text }) => {
     return (
-        <h2>{text}</h2>
+        <h2 className="question">{text}</h2>
     )
 }
 
